225c0067e976a1f6ed45940dca5d66e4472b2130
AinhoaMaya <ainhoamaya.unir@gmail.com>
2024-06-21 12:07:15 +0200

    Creación de la página y componente "home"

---
+++ client/src/components/home.js
@@ -0,0 +1,57 @@
+class Home extends HTMLElement {
+  constructor () {
+    super()
+    this.shadow = this.attachShadow({ mode: 'open' })
+    this.title = this.getAttribute('text')
+  }
+
+  connectedCallback () {
+    this.render()
+  }
+
+  render () {
+    this.shadow.innerHTML =
+      /* html */` 
+      <style>
+      :host {
+        display: block;
+        width: 100%;
+        height: 100vh;
+      }
+
+      .orders-buttons{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+        width: 100%;
+      }
+
+      .button-new-order button, .button-last-order button{
+        padding: 0.6rem;
+        border-radius: 0.7rem;
+        border: none;
+        outline: none;
+        color: hsl(271, 53%, 52%);
+        background-color: hsl(0, 0%, 100%);
+        font-size: 17px;
+        font-weight: 600;
+        min-width: 350px;
+      }
+      </style>
+
+      <nav class="orders">
+        <div class="orders-buttons">
+          <div class="button-new-order">
+            <button>Nuevo pedido</button>
+          </div>
+          <div class="button-last-order">
+            <button>Pedidos anteriores</button>
+          </div>
+        </div>
+      </nav>
+    `
+  }
+}
+
+customElements.define('home-component', Home)
